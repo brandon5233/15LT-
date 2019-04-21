7dc17e925c445d7319c21dd6e4b9e7d06e4ff20c
Brandon Rozario <rozario.brandon@gmail.com>
2019-04-21 18:29:02 +0100

    Implemented Easter Egg !!

--- app/src/main/java/com/dubliners/a15lt/fragment_about.java
+++ app/src/main/java/com/dubliners/a15lt/fragment_about.java
@@ -26,7 +26,26 @@ public class fragment_about extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
-       
+        CardView cardView = view.findViewById(R.id.about_card);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nav_drawer.EasterEggCounter>0)nav_drawer.EasterEggCounter--;
+                if(nav_drawer.EasterEggCounter<=3 && nav_drawer.EasterEggCounter >0){
+                    try{
+                        Toast.makeText(getActivity().getApplicationContext(),
+                                "You are " + nav_drawer.EasterEggCounter+" steps away" +
+                                        "from unlocking the Easter Egg !", Toast.LENGTH_SHORT).show();
+                    }catch (NullPointerException e){e.printStackTrace();}
+                }else if(nav_drawer.EasterEggCounter == 0){
+                    try{
+                        Toast.makeText(getActivity().getApplicationContext(),
+                                "Congratulations!\nYour food votes are now worth 2x !\nHappy Easter!",
+                                Toast.LENGTH_LONG).show();
+                    }catch (NullPointerException e){e.printStackTrace();}
+                }
+            }
+        });
         return view;
 
     }
